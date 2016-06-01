c2cf195271025fe5577c1ab6d92e0a4db914e57e
Jack Moffitt <jack@metajack.im>
2016-05-31 20:24:17 -0600

    WIP native windows port

--- build.rs
+++ build.rs
@@ -2,15 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use std::process::Command;
-use std::env;
-
+extern crate cmake;
 
 fn main() {
-    assert!(Command::new("make")
-        .args(&["-R", "-f", "makefile.cargo", &format!("-j{}", env::var("NUM_JOBS").unwrap())])
-        .status()
-        .unwrap()
-        .success());
-    println!("cargo:rustc-flags=-L native={}", env::var("OUT_DIR").unwrap());
+    let dst = cmake::Config::new(".").build_target("ALL_BUILD").build();
+    println!("cargo:rust-link-search=native={}", dst.display());
+    println!("cargo:rust-link-lib=static=angle");
 }
